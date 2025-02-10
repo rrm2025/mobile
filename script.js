49276d09e2c04ea81bb1c641711c1225191e3c92
rrm2025 <auge00zonasul@gmail.com>
2025-02-10 01:02:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function changeBanner() {
 }
 
 setInterval(changeBanner, 5000); // Troca o banner a cada 5 segundos
+
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
+
+// Fecha o menu se clicar fora dele
+document.addEventListener("click", function(event) {
+    var menu = document.getElementById("menu");
+    var imagemFixa = document.getElementById("imagemFixa");
+
+    if (menu && imagemFixa) {
+        if (!menu.contains(event.target) && event.target !== imagemFixa) {
+            menu.style.display = "none";
+        }
+    }
+});
